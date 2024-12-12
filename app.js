0c3fd72f49d38642b974534068a57f0382cc5121
saadhn4 <saadcfi4@gmail.com>
2024-12-12 12:18:22 +0530

    hashing and verifying

---
+++ app.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcryptjs";
+
+async function hashing() {
+  try {
+    let userPass = "18271";
+    let hashedPass = await bcrypt.hash(userPass, 10);
+    console.log(hashedPass);
+  } catch (error) {
+    console.log(error);
+  }
+}
+hashing();
+// output: $2a$10$DY9m.VJ6HnGwcu3JIGzpzuS2tdcv.eRbN7xsyZWlWIrgOrqPEn1fW
+// $2a$ -> bcrypt version
+// $10$ -> salt value
+// rest is password
+
+//$2a$10$qZDtPxXuPYCmu7/NYlTjRe76h.Z1XtU37IBfHwG.QRXkJE0avjaWy
+
+async function verifyPassword() {
+  try {
+    let hashValue =
+      "$2a$10$qZDtPxXuPYCmu7/NYlTjRe76h.Z1XtU37IBfHwG.QRXkJE0avjaWy";
+    let userPass = "18271";
+    let verify = await bcrypt.compare(userPass, hashValue); //password given should always be first.
+    console.log(verify);
+  } catch (error) {
+    console.log(error);
+  }
+}
+verifyPassword();
